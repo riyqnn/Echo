54779300a76612c9988456b8921c6b524b58fea4
riyan <riyanarhm28@gmail.com>
2025-10-03 18:52:33 +0700

    deploy to mainnet

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -4,10 +4,10 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    u2uTestnet: {
-      url: "https://rpc-nebulas-testnet.u2u.xyz/", 
-      chainId: 2484, 
-      accounts: [process.env.PRIVATE_KEY]
+    u2uMainnet: {
+      url: "https://rpc-mainnet.uniultra.xyz", 
+      chainId: 39, 
+      accounts: [process.env.PRIVATE_KEY] 
     }
   },
   etherscan: {
